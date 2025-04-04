e0d6a68d2e293619842ab55bed76bab1a524f846
Shaik Sameer <109991523+vij-sameerb5@users.noreply.github.com>
2025-04-04 14:35:05 -0400

    Initial commit for Streamlit Mortgage Calculator

---
+++ pages/1_profile.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.write("# 👨‍💻 Profile")
+
+st.markdown("""
+**📧 Email:** sameer.shaik@gwu.edu  
+**?: Github:** [vij-sameerb5](https://github.com/vij-sameerb5)
+**🌐 Website:** [sameer-shaik.com](https://sameer-shaik.com)
+""")
+
+st.write("""
+Hi, I'm **Sameer Shaik**, a graduate student in Computer Science at George Washington University with a passion for building impactful software using AI, machine learning, and intelligent automation. I've participated in 13 hackathons across the U.S. — winning HackPSU and YHacks — and currently serve as the Lead Organizer and Technical Director of George Hacks, where I recently organized a 300-participant hackathon and secured 3 sponsors. With a perfect 4.0 GPA, 3 U.S. internships, and 5 strong letters of recommendation, I’m committed to continuous growth and solving real-world problems through tech.
+
+My interests currently are span from AI Agents and LLM-powered applications to cybersecurity and automation. I've worked on gesture detection for deaf education, LLM-driven chat systems, and 3D interactive web designs using Three.js. I’m always exploring the intersection of creativity and technology — and I’m excited about opportunities that let me blend innovation, community building, and technical depth.
+""")
---
+++ pages/2_dashboard.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.write("# Dashboard")
+st.write("""
+I dont know what to keep here, just leaving it blank for now., used for how to make pages""")
